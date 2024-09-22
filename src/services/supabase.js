f4b574c07e89537f64581600ea782805016aa88c
ROHAN7051 <rohan.sharma7051@gmail.com>
2024-09-22 20:19:34 +0530

    supabase established

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://fjiclmfdzjnmdfdbmolo.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqaWNsbWZkempubWRmZGJtb2xvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY5MDM0OTcsImV4cCI6MjA0MjQ3OTQ5N30.AIgIJWPM99TvKyKBuTNWtTkfRNj2VedcGm9aYfZ_86M";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
