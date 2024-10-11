c35e3565808e368c5ef3f793ebd174cd47b71163
ROHAN7051 <rohan.sharma7051@gmail.com>
2024-10-12 01:26:17 +0530

    client-side sorting sorting cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -9,6 +9,7 @@ function CabinTable() {
   const { isLoading, cabins } = useCabin();
   const [searchParams] = useSearchParams();
   if (isLoading) return <Spinner />;
+  // FOR FILTER
   const filterValue = searchParams.get("discount") || "all";
   console.log(filterValue);
   let filteredCabins;
@@ -17,6 +18,14 @@ function CabinTable() {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // FOR SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -28,7 +37,8 @@ function CabinTable() {
           <div>Discount</div>
         </Table.Header>
         <Table.Body
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 function CabinTableOperations() {
   return (
     <TableOperations>
@@ -9,6 +10,16 @@ function CabinTableOperations() {
           { value: "all", label: "All" },
           { value: "no-discount", label: "No discount" },
           { value: "with-discount", label: "With discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by name (A-Z)" },
+          { value: "name-dsc", label: "Sort by name (Z-A)" },
+          { value: "regularPrice-asc", label: "Sort by price (low first)" },
+          { value: "regularPrice-dsc", label: "Sort by price (high first)" },
+          { value: "maxCapacity-asc", label: "Sort by price (low first)" },
+          { value: "maxCapacity-dsc", label: "Sort by price (high first)" },
         ]}
       />
     </TableOperations>
