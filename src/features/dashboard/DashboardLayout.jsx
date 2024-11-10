4e381141f2815141bb34001ee8f9a59cb71afdc3
ROHAN7051 <rohan.sharma7051@gmail.com>
2024-11-10 14:34:00 +0530

    computing recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,19 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+function DashboardLayout() {
+  const { bookings, isLoading } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+  if (isLoading || isLoading2) return <Spinner />;
+  console.log(bookings);
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
